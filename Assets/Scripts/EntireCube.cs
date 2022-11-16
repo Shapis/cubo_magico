000c628d81f215279a7bf00c20e91460ca0a61bc
Bruno Guimarães <correio.bfg@gmail.com>
2022-11-16 20:52:22 -0300

    Implementar os movimentos

--- Assets/Scripts/EntireCube.cs
+++ Assets/Scripts/EntireCube.cs
@@ -6,6 +6,7 @@ public class EntireCube : MonoBehaviour
 {
     [SerializeField]
     private Material[] m_Colors;
+    private List<Transform> myCubes = new List<Transform>();
 
     // Start is called before the first frame update
     void Start()
@@ -13,7 +14,68 @@ public class EntireCube : MonoBehaviour
         GenerateCube();
     }
 
-    private void RotateFace(int faceNumber) { }
+    private void RotateFace(int faceNumber) {
+        List<Transform> faceCubes = new List<Transform>();
+        if (faceNumber == 0){
+            foreach (var item in myCubes){
+                if (item.position.z <= 0.1f){
+                    item.RotateAround(new Vector3(1f, 1f, 0f), Vector3.forward, 90f);
+                }
+            }
+        }else if (faceNumber == 1){
+            foreach (var item in myCubes){
+                if (item.position.y >= 1.9f){
+                    item.RotateAround(new Vector3(1f, 2f, 1f), Vector3.up, 90f);
+                }
+            }
+        }else if (faceNumber == 2){
+            foreach (var item in myCubes){
+                if (item.position.z >= 1.9f){
+                    item.RotateAround(new Vector3(1f, 1f, 2f), Vector3.forward, -90f);
+                }
+            }
+        }else if (faceNumber == 3){
+            foreach (var item in myCubes){
+                if (item.position.y <= 0.1f){
+                    item.RotateAround(new Vector3(1f, 0f, 1f), Vector3.down, 90f);
+                }
+            }
+        }else if (faceNumber == 4){
+            foreach (var item in myCubes){
+                if (item.position.x <= 0.1f){
+                    item.RotateAround(new Vector3(0f, 1f, 1f), Vector3.right, 90f);
+                }
+            }
+        }else if (faceNumber == 5){
+            foreach (var item in myCubes){
+                if (item.position.x >= 1.9f){
+                    item.RotateAround(new Vector3(2f, 1f, 1f), Vector3.left, 90f);
+                }
+            }
+        }
+        
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.E)){
+            RotateFace(0);
+        }
+        if (Input.GetKeyDown(KeyCode.W)){
+            RotateFace(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Q)){
+            RotateFace(2);
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            RotateFace(3);
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            RotateFace(4);
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            RotateFace(5);
+        }
+    }
 
     void GenerateCube(int cubeSize = 3)
     {
@@ -24,6 +86,7 @@ public class EntireCube : MonoBehaviour
                 for (int z = 0; z < cubeSize; z++)
                 {
                     GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    myCubes.Add(cube.transform);
                     if (x == 0)
                     {
                         var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
